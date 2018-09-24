653e354b7e12c22efc03b5f4076311b77f7964ad
Steven <stevenxchung@gmail.com>
2018-09-24 17:39:54 -0400

    Set up application for NgRx store

--- todoapp-angular-ngrx/src/app/app.module.ts
+++ todoapp-angular-ngrx/src/app/app.module.ts
@@ -1,16 +1,18 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TodoListComponent, TodoListItemComponent],
+  imports: [BrowserModule, FormsModule, NgbModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
