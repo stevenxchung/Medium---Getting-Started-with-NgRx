14de46a1d318c361bee31a49289c8b13d8fd6df3
Steven <stevenxchung@gmail.com>
2018-09-24 19:54:50 -0400

    Add imports to app module

--- todoapp-angular-ngrx/src/app/app.module.ts
+++ todoapp-angular-ngrx/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 
@@ -9,9 +10,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoListItemComponent } from './components/todo-list-item/todo-list-item.component';
 
+import { TodoEffects } from './store/todo/todo.effects';
+import { EffectsModule } from '@ngrx/effects';
+import * as TodoReducer from './store/todo/todo.reducer';
+
 @NgModule({
   declarations: [AppComponent, TodoListComponent, TodoListItemComponent],
-  imports: [BrowserModule, FormsModule, NgbModule.forRoot()],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    StoreModule.forRoot({ todos: TodoReducer.TodoReducer }),
+    EffectsModule.forRoot([TodoEffects])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
